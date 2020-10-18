229b0780dca8d02e50cf6578fb4a62847c0ceb79
Estanis <dawestanis@gmail.com>
2020-10-18 22:22:43 +0100

    "Terminamos Ejercicio (II)"

--- ejercicios_clase/Ejercicios2.js
+++ ejercicios_clase/Ejercicios2.js
@@ -292,4 +292,131 @@ var anio = "2015"
 console.log(dia + "/" + mes + "/" + año);
 console.log(anio + "/" + mes + "/" + dia);
 mes = "Abril";
-console.log(mes + " " + dia + "," + "anio");
+console.log(mes + " " + dia + "," + anio);
+
+/**
+ * ACTIVIDAD 12
+ *  ¿Cuántos segundos han pasado desde que naciste? ¿y minutos? ¿y horas? ¿y días? 
+ * Implementar una función que dada una fecha cualquiera en formato “yyyy-mm-dd”, devuelva 
+ * cuánto tiempo ha pasado desde esa fecha. La función aceptará un segundo parámetro 
+ * para indicar en qué unidad se quiere obtener el resultado: ‘d’ → días, ‘h’ → horas, ‘m’ → minutos y 
+ * ‘s’ → segundos. Si no es ninguna de estas unidades, se mostrará un error.
+ */
+
+// asignar el valor de las unidades en milisegundos
+var msecPerMinute = 1000 * 60;
+var msecPerHour = msecPerMinute * 60;
+var msecPerDay = msecPerHour * 24;
+
+// asignar la fecha en milisegundos
+var date = new Date('23/04/1979');
+var dateMsec = date.getTime();
+
+// asignar la fecha el 1 de enero del a la media noche
+date.setMonth(0);
+date.setDate(1);
+date.setHours(0, 0, 0, 0);
+
+// Obtener la diferencia en milisegundos
+var interval = dateMsec - date.getTime();
+
+// Calcular cuantos días contiene el intervalo. Substraer cuantos días
+// tiene el intervalo para determinar el sobrante
+var days = Math.floor(interval / msecPerDay);
+interval = interval - (days * msecPerDay);
+
+// Calcular las horas , minutos y segundos
+var hours = Math.floor(interval / msecPerHour);
+interval = interval - (hours * msecPerHour);
+
+var minutes = Math.floor(interval / msecPerMinute);
+interval = interval - (minutes * msecPerMinute);
+
+var seconds = Math.floor(interval / 1000);
+
+// Mostrar el resultado.
+document.write(days + " days, " +
+    hours + " hours, " +
+    minutes + " minutes, " +
+    seconds + " seconds.");
+
+/**
+ * ACTIVIDAD 13
+ * Implementar una función hdec2hms(x) que transforme una hora en formato decimal 
+ * a su equivalente en formato hh:mm:ss (en texto). Por ejemplo, 8.25 = “8:15:00” 
+ * y 10.12 = “10:07:12”.Implementar también la función inversa hms2hdec(h,m,s).
+ */
+
+
+
+/**
+ * ACTIVIDAD 14
+ * Como hemos visto, javascript, como muchos otros lenguajes, utilizan el “UNIX EPOCH” 
+ * que cuenta el tiempo a partir de la medianoche del 1 de enero de 1970. 
+ * Sin embargo, hay otros tipos de fecha que tienen otro origen del tiempo. 
+ * Por ejemplo, la fecha juliana empieza a contabilizar el tiempo desde las 12:00 
+ * (mediodía) del 1 de enero de 4713 a.C., e indica el número de días (y fracciones) 
+ * que han transcurrido desde ese momento. La fecha juliana se suele utilizar para 
+ * calcular el tiempo que ha transcurrido desde eventos que sucedieron en la antigüedad 
+ * y es aún ampliamente usada para fenómenos astronómicos e históricos lejanos. 
+ * En la wikipedia (https://es.wikipedia.org/wiki/Fecha_juliana) se puede encontrar 
+ * la relación entre fecha juliana y tiempo Unix (contado a partir desde 
+ * 1/1/1970), siendo esta:
+ * 
+ * fecha_juliana = tiempo_unix + 2440587.5
+ * 
+ * Implementar una función que dada una fecha en texto (por ejemplo “2018-09-20”), 
+ * devuelva la fecha juliana equivalente.
+ * 
+ * Para realizar la suma, ten en cuenta que la fecha juliana almacena días, mientras 
+ * que el tiempo_unix en javascript contiene milisegundos, por lo que hay que 
+ * transformarlo en días. Por cierto, ¿de dónde crees que ha salido el valor 2440587.5?
+ */
+
+
+ function dividirCadena(fecha, separador){ // con esta función eliminamos los guiones de la fecha dada.
+     var arrayDeCadenas = fecha.split(separador);
+     document.write('<p>La cadena original es: "' + fecha + '"');
+     document.write('<br>El separador es: "' + separador + '"');
+     document.write("<br>El array tiene " + arrayDeCadenas.length + " elementos: ");
+
+     for(var i=0;i<arrayDeCadenas.length;i++){
+         document.write(arrayDeCadenas[i] + " / ");
+     }
+ }
+ var fecha = "2018-09-20";
+ var separador = "-";
+
+ dividirCadena(fecha,separador);
+
+
+ /**
+  * ACTIVIDAD 15
+  * Cuando hablamos de ángulos, normalmente utilizamos los grados sexagesimales 
+  * (se suelen representar como “deg”, una circunferencia completa son 360º deg). 
+  * Sin embargo, en trigonometría es más común utilizar los radianes (“rad”, una 
+  * circunferencia completa son 2π rad).
+  * 
+  * De esta forma, 360º equivalen a 2π. Las funciones trigonométricas de javascript, 
+  * como la mayoría de lenguajes, trabajan en radianes, sin embargo en muchas ocasiones 
+  * nosotros vamos a querer trabajaren grados. Por ello, se pide:
+  * 
+  * a) Implementar una función llamada deg2rad(x) que transforme de grados a 
+  * radianes, y su inversa rad2deg(x)
+  * 
+  * b) Implementar una función sinDeg(x)que devuelva el seno del ángulo x y otra 
+  * función cosDeg(x) que devuelva el coseno del ángulo x (en ambos casos x está en 
+  * grados).
+  * 
+  * c) El seno y el coseno están relacionados por la siguiente ecuación: 
+  * sin2x + cos2x = 1.
+  * 
+  * Utilizar esta relación para implementar una función sinDegAlt(x) que obtenga el 
+  * seno de un ángulo x (en grados) usando para ello la función cosDeg(x) anteriormente 
+  * implementada.
+  * 
+  * Implementar también la función inversa al apartado anterior: cosDegAlt(x) usando 
+  * para ello sinDeg(x)
+  */
+
+  

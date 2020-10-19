93dffdf4262ff402e112b63369bcf510e994f4b5
Estanis <dawestanis@gmail.com>
2020-10-19 23:49:43 +0100

    "Realizamos primera actividad del Ejercicio(III)"

---
+++ ejercicios_clase/Ejercicios3.js
@@ -0,0 +1,20 @@
+// EJERCICIOS (III)
+
+/**
+ * ACTIVIDAD 1
+ * Realizar una función que indicando un texto y un carácter (ambos obligatorios, 
+ * comprobar que el segundo parámetro es realmente un único carácter), nos diga 
+ * cuántas veces aparece el carácter en el texto.
+ */
+
+ function detectarCaracter(texto, caracter){
+    var lista = [];
+    var cadena = texto;
+    var letra = caracter;
+    var expresionRegular;
+    expresionRegular = /^\w{1}[a-z,A-Z]$/
+    if(expresionRegular.test(letra)==true){
+        
+    }
+
+ }
